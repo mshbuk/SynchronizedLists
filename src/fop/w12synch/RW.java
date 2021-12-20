7ed93a7b49904f43662eee9023d338a1ab68638f
Mariia Bukiaeva <bukaieva.mariia@kiu.edu.ge>
2021-12-20 15:45:49 +0400

    no message

--- src/fop/w12synch/RW.java
+++ src/fop/w12synch/RW.java
@@ -6,26 +6,26 @@ public class RW {
     private int countReaders = 0;
 
     public synchronized void startRead() throws InterruptedException {
-        while(countReaders <0)
-            wait();
+        while(countReaders < 0)
+            this.wait();
         countReaders++;
     }
 
     public synchronized void endRead() {
         countReaders--;
-        if(countReaders ==0)
-            notify();
-
+        if(countReaders == 0)
+            this.notify();
     }
 
     public synchronized void startWrite() throws InterruptedException {
-        while(countReaders !=0) wait();
+        while(countReaders != 0)
+            this.wait();
         countReaders = -1;
     }
 
     public synchronized void endWrite() {
-        countReaders =0;
-        notifyAll();
+        countReaders = 0;
+        this.notifyAll();
     }
 
 }
