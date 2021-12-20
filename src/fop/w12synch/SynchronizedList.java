339354fc4ba4ad83f51bed6bde4cb8a5df629d23
Mariia Bukiaeva <bukaieva.mariia@kiu.edu.ge>
2021-12-20 16:12:11 +0400

    no message

--- src/fop/w12synch/SynchronizedList.java
+++ src/fop/w12synch/SynchronizedList.java
@@ -2,10 +2,17 @@ package fop.w12synch;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class SynchronizedList<T> {
+   // private ReadWriteLock relock = new ReentrantReadWriteLock();
+
     private List<T> innerList = new LinkedList<>();
+
     private RW rw;
+
     public void add(int index, T e) throws InterruptedException {
         rw.startWrite();
         innerList.add(index, e);
@@ -13,14 +20,23 @@ public class SynchronizedList<T> {
     }
 
     public T remove(int index) throws InterruptedException {
-        return innerList.remove(index);
+        rw.startWrite();
+        T result = innerList.remove(index);
+        rw.endWrite();
+        return result;
     }
 
     public T get(int index) throws InterruptedException {
-        return innerList.get(index);
+        rw.startRead();
+        T result = innerList.get(index);
+        rw.endRead();
+        return result;
     }
 
     public boolean contains(T e) throws InterruptedException {
-        return innerList.contains(e);
+        rw.startRead();
+        boolean result =  innerList.contains(e);
+        rw.endRead();
+        return result;
     }
 }
